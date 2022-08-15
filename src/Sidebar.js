e16109100c2846b44081a52341b9b83a359eed7e
Tom Black <tom.black06007@gmail.com>
2022-08-15 12:04:21 -0600

    Added the bottom portion of the sidebar and made a function to render chunks of html.

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -3,6 +3,13 @@ import React from 'react'
 import './Sidebar.css';
 
 const Sidebar = () => {
+    const recentItem = (topic) => (
+        <div className="sidebar-recentItem">
+        <span className="sidebar-hash">#</span>
+        <p>{topic}</p>
+        </div>
+    );
+    
     return (
         <div className="sidebar">
             <div className="sidebar-top">
@@ -18,12 +25,17 @@ const Sidebar = () => {
                 </div>
                 <div className="sidebar-stat">
                 <p>Views on post</p>
-                <p className="sidebar-statnumber">2,448</p>
+                <p className="sidebar-statNumber">2,448</p>
                 </div>
             </div>
 
             <div className="sidebar-bottom">
                 <p>Recent</p>
+                {recentItem("react.js")}
+                {recentItem("Programming")}
+                {recentItem("SoftwareEngineering")}
+                {recentItem("design")}
+                {recentItem("developer")}
             
             </div>
         </div>
