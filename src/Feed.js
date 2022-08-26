5ea392411afaf9f256929497a2f0ea4d6f4a7fe6
Tom Black <tom.black06007@gmail.com>
2022-08-26 11:17:23 -0700

    Added the search bar to the feed section.

--- src/Feed.js
+++ src/Feed.js
@@ -1,10 +1,25 @@
+import CreateIcon from '@mui/icons-material/Create';
 import React from 'react';
 import './Feed.css'
 
 const Feed = () => {
-  return (
-    <div>Feed</div>
-  )
+    return (
+        <div className="feed">
+            <div className="feed-inputContainer">
+                <div className="feed-input">
+                    <CreateIcon />
+                    <form>
+                    <input type='text'></input>
+                    <button type='submit'>Submit</button>
+                    </form>
+                </div>
+                <div className="feed-inputOptions">
+                {/* Options */}
+                
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Feed
