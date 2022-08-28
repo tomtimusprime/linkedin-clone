76342561a4b231d794c45e17035cd1efeb6fa1a5
Tom Black <tom.black06007@gmail.com>
2022-08-28 09:30:38 -0700

    Added the InputOption component and added icons.

--- src/Feed.js
+++ src/Feed.js
@@ -1,6 +1,11 @@
 import CreateIcon from '@mui/icons-material/Create';
+import ImageIcon from '@mui/icons-material/Image';
+import OnDemandVideoIcon from '@mui/icons-material/OndemandVideo';
+import EventNoteIcon from '@mui/icons-material/Event';
+import ArticleIcon from '@mui/icons-material/Article';
 import React from 'react';
 import './Feed.css'
+import InputOption from './InputOption';
 
 const Feed = () => {
     return (
@@ -15,7 +20,10 @@ const Feed = () => {
                 </div>
                 <div className="feed-inputOptions">
                 {/* Options */}
-                
+                <InputOption Icon={ImageIcon} title='Photo' color='#70b5f9'/>
+                <InputOption Icon={OnDemandVideoIcon} title='Video' color='#5f9b41'/>
+                <InputOption Icon={EventNoteIcon} title='Event' color='#c0cbcd'/>
+                <InputOption Icon={ArticleIcon} title='Write article' color='#e16745'/>
                 </div>
             </div>
         </div>
